c6c8a4b072c1c244c177a6effe172787959d99e4
Miranda Helck <miranda.helck@gmail.com>
2020-02-21 00:20:19 -0500

    Edits for Portfolio v2.0
    Complete redesign of portfolio

---
+++ portfolio_script.js
@@ -0,0 +1,28 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function myFunction() {
+  var x = document.getElementById("nav_menu");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
